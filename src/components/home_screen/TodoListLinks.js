749fb42c19ff7ccbe37df149b0d5ecfe373cb4ec
SolarisTKW <mcng555@gmail.com>
2019-11-17 20:36:55 -0500

    handle register

--- src/components/home_screen/TodoListLinks.js
+++ src/components/home_screen/TodoListLinks.js
@@ -6,11 +6,8 @@ import TodoListCard from './TodoListCard';
 
 class TodoListLinks extends React.Component {
 
-    //
-    
     render() {
         const todoLists = this.props.todoLists;
-        // console.log(todoLists);
         return (
             <div className="todo-lists section">
                 {todoLists && todoLists.map(todoList => (
